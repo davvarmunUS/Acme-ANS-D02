b22fe5d9d8f56d7fb2974ae391315a4d902e94a8
david <david@David.technicolor.net>
2025-03-03 22:41:09 +0100

    feat: Airline

---
+++ src/main/java/acme/entities/airports/Airline.java
@@ -0,0 +1,71 @@
+
+package acme.entities.airports;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.client.components.validation.ValidUrl;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Airline extends AbstractEntity {
+
+	// Serialisation version ---------------------------------------------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes --------------------------------------------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@Automapped
+	private String				code;
+
+	@Mandatory
+	@ValidUrl
+	@Automapped
+	private String				website;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Type				type;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				foundationMoment;
+
+	@Optional
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private String				phoneNumber;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------------------------------------------
+
+}
