e1d75ac85c0c486cd8a3afd66d6f0ed170d38f7f
david <david@David.technicolor.net>
2025-03-10 22:29:45 +0100

    test: Leg Sample Data

--- src/main/java/acme/realms/managers/Manager.java
+++ src/main/java/acme/realms/managers/Manager.java
@@ -18,12 +18,14 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidManager;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidManager
 public class Manager extends AbstractRole {
 
 	// Serialisation version ---------------------------------------------------------------------------------------
